cc4488143dbdfd1f06498a9ad38fe622742130d9
sakitasaka <sakitasaka@hotmail.com>
2024-11-12 14:03:23 -0800

    buggy

---
+++ app/src/main/java/com/example/datesafe/GovernmentId.java
@@ -0,0 +1,12 @@
+package com.example.datesafe;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GovernmentId extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_government_id); // Link it to the new layout
+    }
+}
