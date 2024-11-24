181547c5d084bcaff178f54519523808d472bde3
Joaquin <joaquinaw88@gmail.com>
2024-11-24 14:59:41 -0800

    added live photo instence

--- app/src/main/java/com/example/datesafe/LivePhoto.java
+++ app/src/main/java/com/example/datesafe/LivePhoto.java
@@ -16,12 +16,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class GovernmentId extends AppCompatActivity {
+public class LivePhoto extends AppCompatActivity {
     private static final int CAMERA_REQUEST_CODE = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_government_id);
+        setContentView(R.layout.activity_live_photo);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
@@ -34,11 +34,6 @@ public class GovernmentId extends AppCompatActivity {
             startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
         });
 
-        findViewById(R.id.uploadPhotoButton).setOnClickListener(v -> {
-            // Simulate uploading a photo
-//            Intent intent = new Intent(UploadActivity.this, PicturesMatchActivity.class);
-//            startActivity(intent);
-        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -47,9 +42,6 @@ public class GovernmentId extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             //imageView.setImageBitmap(photo); // Display the captured photo
-            Intent newIntent = new Intent(GovernmentId.this, LivePhoto.class);
-//            newIntent.putExtra("image", photo);
-            startActivity(newIntent);
         }
     }
 }
